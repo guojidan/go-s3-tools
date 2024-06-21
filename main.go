910eed6d02dded1779630267e03de3861432ac62
junxiang Mu <1948535941@qq.com>
2024-06-21 11:23:58 +0800

    config: add config file support
    Signed-off-by: junxiang Mu <1948535941@qq.com>

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
@@ -12,6 +13,24 @@ import (
 
 	"github.com/sirupsen/logrus"
 )
+
+// config
+type Config struct {
+	EndPoint 		string 	`json:"EndPoint"`
+	AccessKeyID 	string 	`json:"AccessKeyID"`
+	SecretAccessKey string 	`json:"SecretAccessKey"`
+	BukcetName 		string 	`json:"BukcetName"`
+	FilePath 		string 	`json:"FilePath"`
+	Debug 			bool 	`json:"Debug"`
+	Test 			bool 	`json:"Test"`
+	NeedPrepareData bool 	`json:"NeedPrepareData"`
+	ListBucket 		bool 	`json:"ListBucket"`
+	Prefix 			string 	`json:"Prefix"`
+	NumThread 		int 	`json:"NumThread"`
+}
+
+// global config
+var config Config
 
 func test_func(minioClient *client.Client) {
 	file_path := "./test_file"
@@ -77,59 +96,72 @@ func run(minioClient *client.Client, bucket_name *string, file_path *string, num
 	wg.Wait()
 }
 
+func load_config_from_file(config_file *string) error {
+	file, err := os.Open(*config_file)
+    if err != nil {
+        fmt.Println("open config file error please check, Error:", err)
+        return err
+    }
+    defer file.Close()
+
+	err = json.NewDecoder(file).Decode(&config)
+	if err != nil {
+		fmt.Println("can not decode config file, Err: ", err)
+		return err
+	}
+
+	return nil
+}
+
 func main() {
 	// parse args
-
-	endpoint := flag.String("endpoint", "", "endpoint for storage")
-	accessKeyID := flag.String("accessKeyID", "", "accessKeyID")
-	secretAccessKey := flag.String("secretAccessKey", "", "secretAccessKey")
-	bukcetName := flag.String("bukcetName", "", "bukcetName")
-	filePath := flag.String("filePath", "", "remove object name list")
-	debug := flag.Bool("debug", false, "debug, will output debug log")
-	test := flag.Bool("test", false, "test if s3 is available")
-	need_prepare_data := flag.Bool("need_prepare_data", false, "update 1000 object")
-	list_bucket := flag.Bool("list_bucket", false, "list bucket object")
-	prefix := flag.String("prefix", "", "list object, object prefix")
-	num_thread := flag.Int("num_thread", 3, "number of thread")
+	config_file := flag.String("config_file", "", "config file path")
 
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "Usage of go-s3-tools:\n")
 		flag.PrintDefaults()
 	}
-
+	
 	flag.Parse()
+	
+	if *config_file != "" {
+		err := load_config_from_file(config_file)
+		if err != nil {
+			return
+		}
+	}
 
-	if *endpoint == "" || *accessKeyID == "" || *secretAccessKey == "" {
+	if config.EndPoint == "" || config.AccessKeyID == "" || config.SecretAccessKey == "" {
 		logrus.Fatalln("args err, please run '-h' check usage")
 		return
 	}
 
-	if *debug {
+	if config.Debug {
 		logrus.SetLevel(logrus.DebugLevel)
 	} else {
 		logrus.SetLevel(logrus.FatalLevel)
 	}
 
-	minioClient, err := client.NewClient(endpoint, accessKeyID, secretAccessKey)
+	minioClient, err := client.NewClient(&config.EndPoint, &config.AccessKeyID, &config.SecretAccessKey)
 	if err != nil {
 		return
 	}
 
-	if *test {
+	if config.Test {
 		test_func(minioClient)
 		return
 	}
 
-	if *list_bucket {
-		minioClient.List_object(bukcetName, prefix)
+	if config.ListBucket {
+		minioClient.List_object(&config.BukcetName, &config.Prefix)
 		return
 	}
 
-	if *need_prepare_data {
-		file.Prepare_test_data(minioClient, bukcetName, filePath)
+	if config.NeedPrepareData {
+		file.Prepare_test_data(minioClient, &config.BukcetName, &config.FilePath)
 	}
 
-	run(minioClient, bukcetName, filePath, *num_thread)
+	run(minioClient, &config.BukcetName, &config.FilePath, config.NumThread)
 
 	logrus.Infoln("finish op")
 }
